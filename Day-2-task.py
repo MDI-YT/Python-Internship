0ce59dee7b00dad1377368a28d3746fc47c60f13
MDI-YT <pabi.adn1@gmail.com>
2020-11-24 14:05:34 +0530

    Update Day-2-task.py

---
+++ Day-2-task.py
@@ -0,0 +1,23 @@
+#task1
+a=b=c=30
+a//=10
+b*=50
+c+=60
+print(a,b,c)
+#output: 3 1500 90
+
+
+#task2
+str1= "Rainy"
+str1= str1[:2] + 'G' + str1[3:]
+print(str1)
+#output: RaGny
+
+#task3
+a = 3
+b=10.5
+a=float(a)
+b=int(b)
+print(a,b)
+#output: 3.0 10
+
