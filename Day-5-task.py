b3e686bfd568d9e60120b0092337dac6a92d0276
MDI-YT <pabi.adn1@gmail.com>
2020-11-27 12:26:14 +0530

    Create Day-5-task.py

---
+++ Day-5-task.py
@@ -0,0 +1,23 @@
+#task1
+def getinput():
+	a=int(input()) #2
+	b=int(input()) #3
+	print("Addition of two numbers:",a+b)
+	#output Addition of two numbers:5
+	print("Subtraction of two numbers:",a-b)
+	#output Subtraction of two numbers:-1
+	print("Multiplication of two numbers:",a*b)
+	#output Multiplication of two numbers:6
+	print("Division of two numbers:",a//b)
+	#output Division of two numbers:0
+
+getinput()
+
+#task2
+def covid(patient_name,body_temperature=98):
+	pass
+
+patient_name=input()
+covid(patient_name)
+
+
